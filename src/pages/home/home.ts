f4dce87248daec1f33a61818dc3d4dfc23fa7ded
Dana Tang <dgtang@eagles.usi.edu>
2017-12-19 22:55:13 -0600

    adding scan for peripherals

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,59 @@
 import { Component } from '@angular/core';
 import { NavController,Platform } from 'ionic-angular';
 import { BLE } from '@ionic-native/ble';
-import { LightBlueService } from 'ionic-lightblue';
+//import { LightBlueService } from 'ionic-lightblue';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+   LIGHTBLUE_NAME = 'BeanName';
+   OPTIONAL_END_SERIAL_RESPONSE_CHAR = "\n";
 
-  constructor(public navCtrl: NavController, private ble: BLE,public platform: Platform,
-              private lightblue: LightBlueService) {
+  constructor(public navCtrl: NavController, private ble: BLE,public platform: Platform
+  /*private lightblue: LightBlueService*/) {
+              /*  platform.ready().then(() => {
+      console.log('platform ready');
 
+      // OPTIONAL_END_SERIAL_RESPONSE_CHAR used when sendSerial isWaitResponse = true
+      this.lightblue.connect(LIGHTBLUE_NAME, OPTIONAL_END_SERIAL_RESPONSE_CHAR).subscribe(next => {
+          console.log("connected");
+
+          this.lightblue.readSerial().subscribe( next => {
+              console.log(next);
+          },
+          error => {
+            console.log(error.toString());
+          })
+      },
+      error => {
+        console.log(error.toString());
+      });
+    });
+  }
+
+  onSend() {
+    console.log("send");
+    // Can wait or not wait response
+    this.lightblue.sendSerial("command", true).subscribe(next => {
+      console.log(next);
+    },
+    error => {
+      console.log(error.toString());
+    });
+
+  }*/
+
+  }
+  scan(services, seconds){
+    services = [];
+    seconds = 100;
+
+  }
+
+  stopScan(){
 
   }
 
