313f73f8c8758f5207ecf10d772e365a99f52e24
Dana Tang <dgtang@eagles.usi.edu>
2018-01-03 14:36:20 -0600

    device motion plan

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -2,6 +2,7 @@ import { Component, NgZone } from '@angular/core';
 import { NavController,Platform, NavParams} from 'ionic-angular';
 import { BLE } from '@ionic-native/ble';
 import { ToastController } from 'ionic-angular';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion';
 
 @Component({
   selector: 'details-page',
@@ -17,7 +18,7 @@ export class DetailsPage {
    deviceDetails;
 
 
-  constructor(public navCtrl: NavController, private ble: BLE,public platform: Platform, private toastCtrl: ToastController, private ngZone: NgZone, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, private ble: BLE,public platform: Platform, private toastCtrl: ToastController, private ngZone: NgZone, public navParams: NavParams, private deviceMotion: DeviceMotion) {
   //https://github.com/don/ionic-ble-examples/blob/master/scan/src/pages/home/home.ts
 
 //logging right thing so something is wrong with theis
@@ -34,53 +35,15 @@ export class DetailsPage {
   console.log(this.navParams.get('id'));
   // this.deviceDetails = this.navParams.get('device.id');
   this.deviceDetails = this.navParams.get('id');
+
+  //search for device with this id
+  //connect to that deivce
+  //display its current accelerometer data
+  //store it accelerometer data
+  //put accelerometer data into an equation that outputs velocity
+  //put velocity in an equation that outputs miles per hour
+  //graph data
+
   }
-  /*ionViewDidEnter() {
-    console.log('ionViewDidEnter');
-    this.scan();
-  }
-
-  scan() {
-    this.setStatus('Scanning for Bluetooth LE Devices');
-    this.devices = [];  // clear list
-    console.log('here1');
-    this.ble.scan([], 5).subscribe(
-      device => this.onDeviceDiscovered(device),
-      error => this.scanError(error)
-    );
-
-    console.log('here2');
-    setTimeout(this.setStatus.bind(this), 5000, 'Scan complete');
-  }
-
-  onDeviceDiscovered(device) {
-    console.log('Discovered ' + JSON.stringify(device, null, 2));
-    this.ngZone.run(() => {
-      this.devices.push(device);
-    });
-  }
-
-  // If location permission is denied, you'll end up here
-  scanError(error) {
-    this.setStatus('Error ' + error);
-    let toast = this.toastCtrl.create({
-      message: 'Error !!',
-      position: 'middle',
-      duration: 5000
-    });
-    toast.present();
-  }
-
-  setStatus(message) {
-    console.log(message);
-    this.ngZone.run(() => {
-      this.statusMessage = message;
-    });
-  }
-
-  choose(){
-    this.navCtrl.push(Details);
-  }*/
-
 
 }
